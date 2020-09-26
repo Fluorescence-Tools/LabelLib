5efd0c21e10bb364e346a54ddbd9ce5ca5565085
Mykola Dimura <mykola.dimura@gmail.com>
2020-09-26 19:20:01 +0200

    Add pypi deployment to TravisCI

--- build-wheels.sh
+++ build-wheels.sh
@@ -6,13 +6,15 @@
 #git clone --recursive https://github.com/Fluorescence-Tools/LabelLib.git
 #docker run -e PYPI_USER=***** -e PYPI_PASS=***** --rm -v `pwd`/LabelLib:/io:rw quay.io/pypa/manylinux1_x86_64 /io/build-wheels.sh
 
+VERSION_PREFIX=$1 # Could be `2` for python 2.*, `3` for python 3.x, `35` for python 3.5, etc; leave empy for all versions
+
 PYBIN=/opt/python/cp37-cp37m/bin
 "${PYBIN}/pip" install cmake
 ln -s /opt/_internal/*/bin/cmake /usr/bin/cmake
 "${PYBIN}/pip" install twine
 
 # Compile wheels
-for PYBIN in /opt/python/*/bin; do
+for PYBIN in /opt/python/cp${VERSION_PREFIX}*/bin; do
     "${PYBIN}/pip" install numpy
     "${PYBIN}/pip" wheel /io/ -w wheelhouse/
 done
@@ -33,6 +35,5 @@ repository: https://upload.pypi.org/legacy/
 username: $PYPI_USER
 password: $PYPI_PASS
 EOF
-( cd /io; git submodule update --init; ${PYBIN}/python3 setup.py sdist)
-/opt/_internal/*/bin/twine upload /io/wheelhouse/* /io/dist/*.tar.gz
+/opt/_internal/*/bin/twine upload /io/wheelhouse/*
 fi
