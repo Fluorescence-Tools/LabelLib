203a7d9db2f950c4b17739ec2be4633a49013be2
Mykola Dimura <12734546+mdimura@users.noreply.github.com>
2020-11-02 17:10:30 +0100

    Add python ver. 3.8.6 to Windows wheels

--- build_win.sh
+++ build_win.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 set -e
-PY_VERSION_LIST='3.5.4 3.6.8 3.7.9 ""'
+PY_VERSION_LIST='3.5.4 3.6.8 3.7.9 3.8.6 ""'
 mkdir C:/wheelhouse
 for PY_VERSION in $PY_VERSION_LIST; do
     choco install python --version=$PY_VERSION -y
